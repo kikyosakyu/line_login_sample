7a4efe07b4f347e030b2d4099434236e990d4a6e
kikyosakyu <lucille1949.riley@gmail.com>
2020-08-29 22:12:15 +0900

    modified

---
+++ src/components/Home.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect} from 'react'
+import Cokkies from 'js-cookie'
+import {environment} from '../environment'
+import axios from 'axios'
+
+const Home = () => {
+
+  
+  useEffect(()=>{
+    const query = window.location.search.slice(1)
+    const queries = {}
+    
+    if (!query) {
+      window.location.href = window.location.origin
+    } else {
+      query.split('&').forEach((query) =>  {
+        var queryArr = query.split('=');
+        queries[queryArr[0]] = queryArr[1];
+      });
+    }
+
+    const responded_error = queries['error'];
+    const responded_error_description = queries['error_description'];
+
+    if (responded_error) {
+      console.error(responded_error_description);
+      window.location.href = window.location.origin
+    }
+
+    const code = queries['code'];
+    const state = queries['state'];
+
+    if (state !== Cokkies.get('auth_state')) {
+      console.error('state is wrong');
+      window.location.href = window.location.origin
+    }
+
+    const redirect_uri = window.location.origin
+
+    const url = `${environment.cloud_functions.host_name}/lineLogin`
+    const body = {
+      code: code,
+      redirect_uri: redirect_uri
+    }
+
+    axios.post(url, body).then(responce => {
+      alert(responce)
+    })
+    
+    
+  }, [])
+  
+  return(
+    <div>
+      {window.location.search}
+      {(Cokkies.get('auth_state'))}
+    </div>
+  )
+}
+
+
+export default Home
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {environment} from '../environment'
+import Cookies from 'js-cookie'
+
+const SignIn = () => {
+
+  const handleClick = () => {
+    let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+    for (let i = 0, len = chars.length; i < len; i++) {
+        switch (chars[i]) {
+            case "x":
+                chars[i] = Math.floor(Math.random() * 16).toString(16);
+                break;
+            case "y":
+                chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                break;
+        }
+    }
+    
+    const end_point = 'https://access.line.me/oauth2/v2.1/authorize'
+    const response_type = encodeURIComponent('code')
+    const client_id = environment.line.login.channel_id
+    const redirect_uri = encodeURIComponent(window.location.href + 'home')
+    const state = chars.join("")
+    const scope = encodeURIComponent('openid profile')
+    const url = `${end_point}?response_type=${response_type}&client_id=${client_id}&`
+          + `redirect_uri=${redirect_uri}&state=${state}&scope=${scope}`;
+
+    Cookies.set('auth_state', state)
+    
+    window.location.href = url
+  }
+  return (
+    <button onClick={handleClick}>
+      sign in
+    </button>
+  )
+}
+
+export default SignIn
